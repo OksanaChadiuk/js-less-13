d036eb0d0cc164121758b300df51bee8240e022b
Oksana Chadiuk <oksanachadiuk@gmail.com>
2023-10-03 21:15:28 +0300

    add-task1

---
+++ test-1.js
@@ -0,0 +1,8 @@
+//(Age verification) 
+const userAge = prompt("Введіть ваш вік", "");
+
+if (userAge >= 18){
+    alert ("Користувач повнолітній");
+}else {
+    alert ("Користувач не повнолітній");
+}
